bbeab7e2a41f01ad6b0921d36f3caac41020e0ca
atef772356 <atef78975@gmail.com>
2026-03-31 11:28:41 +0200

    Update route.ts

--- src/app/api/[storeId]/orders/[orderId]/route.ts
+++ src/app/api/[storeId]/orders/[orderId]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import prismadb from "../../../../../../lib/prismadb";
-
+export const dynamic = "force-dynamic";
 export async function PATCH(
   req: Request,
   { params }: { params: Promise<{ storeId: string; orderId: string }> },
