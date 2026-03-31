df5c20a3080ec175a830aa900f7bf73c0d73411f
atef772356 <atef78975@gmail.com>
2026-03-31 11:30:10 +0200

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { stripe } from "../../../../lib/stripe";
 import prismadb from "../../../../lib/prismadb";
-
+export const dynamic = "force-dynamic";
 export async function POST(req: Request) {
   const body = await req.text();
 
