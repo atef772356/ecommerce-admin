3858577260ed95abf05c30950671db005e0343ad
atef772356 <atef78975@gmail.com>
2026-03-31 11:26:49 +0200

    Update route.ts

--- src/app/api/[storeId]/categories/route.ts
+++ src/app/api/[storeId]/categories/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import prismadb from "../../../../../lib/prismadb";
-
+export const dynamic = "force-dynamic";
 // 1. إنشاء فئة جديدة (POST)
 export async function POST(
   req: Request,
