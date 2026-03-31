a1425333101f8a7260ba0c4d0eca8d7ec15ed599
atef772356 <atef78975@gmail.com>
2026-03-31 11:29:37 +0200

    Update route.ts

--- src/app/api/stores/route.ts
+++ src/app/api/stores/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 import prismadb from "../../../../lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
-
+export const dynamic = "force-dynamic";
 export async function POST(req: Request) {
   try {
     // 1. التحقق من المستخدم (Hiring Security)
