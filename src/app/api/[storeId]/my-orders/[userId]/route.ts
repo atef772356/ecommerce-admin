3e818e10dea36ff00aaa2c557072b780b38e107b
atef772356 <atef78975@gmail.com>
2026-03-31 11:28:22 +0200

    Set dynamic response to force-dynamic

--- src/app/api/[storeId]/my-orders/[userId]/route.ts
+++ src/app/api/[storeId]/my-orders/[userId]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import prismadb from "../../../../../../lib/prismadb";
-
+export const dynamic = "force-dynamic";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, OPTIONS",
